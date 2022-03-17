87e96315f196b30bb55d7ca564f5f3395beba882
onesmus-hub <78600209+onesmus-hub@users.noreply.github.com>
2022-03-17 06:47:15 +0000

    Delete Back end directory

--- Back end/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# from . import models
-from .models import Client
-from .models import Shop
-# Register your models here.
-
-admin.site.register(Client)
-admin.site.register(Shop)
